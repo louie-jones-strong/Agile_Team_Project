8d58bec28b310c545b3dc8268ca7545bfd0d9a77
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2022-01-23 15:38:38 +0000

    improve home page look

--- routes/main.js
+++ routes/main.js
@@ -8,26 +8,51 @@ module.exports = function(app, port)
 
 	app.get("/",function(req, res)
 	{
-		res.render("home.ejs", {PageData: {IsDevMode: IsDevMode}})
+		res.render("home.ejs", {
+			PageData: {
+				IsDevMode: IsDevMode,
+				PageName: "home",
+			}
+		});
 	});
 
 	app.get("/about",function(req, res)
 	{
-		res.render("about.ejs", {PageData: {IsDevMode: IsDevMode}});
+		res.render("about.ejs", {
+			PageData: {
+				IsDevMode: IsDevMode,
+				PageName: "about",
+			}
+		});
 	});
 
 	app.get("/calendar",function(req, res)
 	{
-		res.render("calendar.ejs", {PageData: {IsDevMode: IsDevMode}})
+		res.render("calendar.ejs", {
+			PageData: {
+				IsDevMode: IsDevMode,
+				PageName: "calendar",
+			}
+		});
 	});
 
 	app.get("/clocks",function(req, res)
 	{
-		res.render("clocks.ejs", {PageData: {IsDevMode: IsDevMode}});
+		res.render("clocks.ejs", {
+			PageData: {
+				IsDevMode: IsDevMode,
+				PageName: "clocks",
+			}
+		});
 	});
 
 	app.get("/timeline",function(req, res)
 	{
-		res.render("timeline.ejs", {PageData: {IsDevMode: IsDevMode}});
+		res.render("timeline.ejs", {
+			PageData: {
+				IsDevMode: IsDevMode,
+				PageName: "timeline",
+			}
+		});
 	});
 }
