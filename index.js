8a70773c25d311889d72620dfc35328214fbf8f8
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2022-02-20 18:34:53 +0000

    improve logging by moving it to middle ware

--- index.js
+++ index.js
@@ -24,22 +24,46 @@ db.connect((err) => {
 });
 global.db = db;
 
-app.use(expressSanitizer());
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set("view engine", "ejs");
-app.engine("html", require("ejs").renderFile);
+
+
 
 // setup static folder for page assets eg: css, images and icon
 app.use(express.static('public'));
 
-// logic for each end point
-require("./routes/main")(app, port);
+app.use(expressSanitizer());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(LogRequest);
+
+// logic for each end point
+require("./routes/main")(app);
+
+
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 // html templates for each page
 app.set("views",__dirname + "/views");
+
+
 
 // start server
 app.listen(port, () => {
 	console.log(`Node server listening on port ${port}!`);
 	console.log(`http://localhost:${port}/`);
 });
+
+
+function LogRequest(req, res, next)
+{
+	let startTime = performance.now();
+
+	console.log();
+	console.log(req.method, req._parsedUrl.pathname, req.query, req.body);
+	next();
+
+	console.log(res.statusCode, `took: ${performance.now() - startTime} ms`);
+	if (res.statusCode >= 300)
+	{
+		console.log(res.statusMessage)
+	}
+}
